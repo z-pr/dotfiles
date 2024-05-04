767e584f206a3c9ad9ff790386985c4c84d4f532
zperekoty <zakharperekoty@gmail.com>
2024-05-04 11:12:56 +0300

    golines impr

--- .config/nvim/lua/plugins/overrides.lua
+++ .config/nvim/lua/plugins/overrides.lua
@@ -517,7 +517,7 @@ return {
 				-- Disable "format_on_save lsp_fallback" for languages that don"t
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = {}
 				return {
 					timeout_ms = 500,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -526,6 +526,8 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = { "gofumpt", "goimports-reviser", "golines" },
+				c = { "clangd" },
+				cpp = { "clangd" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
@@ -536,7 +538,7 @@ return {
 			formatters = {
 				golines = {
 					command = "golines",
-					args = { "-m", "70" },
+					args = { "-m", "80" },
 					stdin = true,
 				},
 			},
